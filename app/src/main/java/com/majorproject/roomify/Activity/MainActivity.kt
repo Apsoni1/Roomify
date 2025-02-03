e061ae3a11b9775215406974588cf775d4c9bd5f
Apsoni1 <apoorvsoni587@gmail.com>
2025-02-03 22:49:22 +0530

    checking merge

--- app/src/main/java/com/majorproject/roomify/Activity/MainActivity.kt
+++ app/src/main/java/com/majorproject/roomify/Activity/MainActivity.kt
@@ -18,6 +18,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var category: Category
     private lateinit var cart: Cart
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
