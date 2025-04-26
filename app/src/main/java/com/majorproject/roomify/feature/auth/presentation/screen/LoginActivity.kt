8510c0bd09104a465b8b588509598e9391b218ec
Anukriti-j <anu200307@gmail.com>
2025-04-26 11:38:47 +0530

    Login and Register page UI added

---
+++ app/src/main/java/com/majorproject/roomify/feature/auth/presentation/screen/LoginActivity.kt
@@ -0,0 +1,11 @@
+package com.majorproject.roomify.feature.auth.presentation.screen
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.majorproject.roomify.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
